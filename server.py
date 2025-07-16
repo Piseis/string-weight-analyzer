eccb8302a563c76595c9b2b2049bfa382b4ebaa8
Piseis <josevilaseca103@gmail.com>
2025-07-16 12:06:11 -0400

    feat: create class StringProcessor  to calculate string weight

--- server.py
+++ server.py
@@ -7,3 +7,15 @@ logging.basicConfig(
     filemode='w'
 )
 logger = logging.getLogger(__name__)
+
+
+class StringProcessor:
+
+    @staticmethod
+    def calculate_weight(string):
+        letters = sum(c.isalpha for c in string)
+        numbers = sum(c.isdigit for c in string)
+        spaces = string.count(' ')
+
+        return (letters * 1.5 + numbers * 2) / spaces
+    
