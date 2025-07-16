6929e2aff619fe66ee9c7847fd517bc717f4c28a
Piseis <josevilaseca103@gmail.com>
2025-07-16 12:59:41 -0400

    feat: implement Server class for client data handling with multithreading support
    - Handles receiving, processing and retrieving client data
    - Uses threads to manage multiple concurrent clients

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
 import logging
+import time
+import socket
+from threading import Thread
 
 logging.basicConfig(
     level=logging.INFO,
@@ -19,3 +22,36 @@ class StringProcessor:
 
         return (letters * 1.5 + numbers * 2) / spaces
     
+
+class Server:
+    def __init__(self, host='localhost', port=65432):
+        self.host = host
+        self.port = port
+        self.processor = StringProcessor()
+
+    def handle_client(self, conn, address):
+        with conn:
+            logger.info(f'Connected by {address}')
+            start_time = time.time()
+
+            while True:
+                data = conn.recv(1024).decode('utf-8')
+                if not data:
+                    break
+
+                weight = self.processor.calculate_weight(data)
+                conn.sendall(str(weight).encode('utf-8'))
+            
+            elapsed = time.time() - start_time
+            logger.info(f'Process completed in {elapsed} seconds.')
+    
+    def start(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((self.host, self.port))
+            s.listen()
+            logger.info(f'Server started in {self.host}:{self.port}')
+
+            while(True):
+                conn, address = s.accept()
+                thread = Thread(target=self.handle_client, args=(conn, address))
+                thread.start()
