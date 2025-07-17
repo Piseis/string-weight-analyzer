84d8ca58ad5a6c7bfd8be082f4203bd27d3e8261
Piseis <josevilaseca103@gmail.com>
2025-07-16 21:44:55 -0400

    fix: handle last line processing in generated file

--- server/models/string_processor.py
+++ server/models/string_processor.py
@@ -2,9 +2,13 @@ class StringProcessor:
 
     @staticmethod
     def calculate_weight(string):
-        letters = sum(c.isalpha for c in string)
-        numbers = sum(c.isdigit for c in string)
+        
+        letters = sum(c.isalpha() for c in string)
+        numbers = sum(c.isdigit() for c in string)
         spaces = string.count(' ')
+
+        if spaces == 0:
+            return 0
 
         return (letters * 1.5 + numbers * 2) / spaces
     
