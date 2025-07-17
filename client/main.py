1b1ae127bae3b63e5e63db6a23b7f6c2ed210a7b
Piseis <josevilaseca103@gmail.com>
2025-07-16 22:26:36 -0400

    feat: add customizable network settings
    - Make ports configurable for both client and server
    - Allow host location configuration on client

--- client/main.py
+++ client/main.py
@@ -4,8 +4,10 @@ from .models.client import StringClient
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="String Processing Client")
     parser.add_argument("-n", "--num_strings", type=int, default=1_000_000, help="Number of strings to generate")
+    parser.add_argument("-ht", "--host", type=str, default='localhost', help="Host of server application")
+    parser.add_argument("-p", "--port", type=int, default=65432, help="Port of server application")
     args = parser.parse_args()
 
-    client = StringClient()
+    client = StringClient(host=args.host, port=args.port)
     client.genereate_file(args.num_strings)
     client.process_strings()
--- server/main.py
+++ server/main.py
@@ -1,5 +1,11 @@
+import argparse
 from server.models.server import Server
 
 if __name__ == "__main__":
-    server = Server()
+
+    parser = argparse.ArgumentParser(description="String Processing Server")
+    parser.add_argument("-p", "--port", type=int, default=65432, help="Port number to run the application")
+    args = parser.parse_args()
+
+    server = Server(port=args.port)
     server.start()
